d77a3190b4c138eeb4590c90e3e6ceea0b56a446
Galitsin Nikolay <NGalitsin@yandex.com>
2023-05-21 15:02:36 +0300

    created user model, 3 types proxy user models and 3 types user profile models

--- createdin/createdin/settings.py
+++ createdin/createdin/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+AUTH_USER_MODEL = 'users.User'
